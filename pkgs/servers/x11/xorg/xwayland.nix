83a819aa8257bb7174eeb25816e2c17ee5f1fc67
Rouven Czerwinski <rouven@czerwinskis.de>
2021-03-09 16:20:55 +0100

    xwayland: meson build system for next release
    The next xwayland release will not be done in tandem with the xserver
    [1], prepare the xwayland derivation for this.
    
    [1]: https://gitlab.freedesktop.org/xorg/xserver/-/merge_requests/582
    
    Signed-off-by: Rouven Czerwinski <rouven@czerwinskis.de>

--- pkgs/servers/x11/xorg/xwayland.nix
+++ pkgs/servers/x11/xorg/xwayland.nix
@@ -1,30 +1,27 @@
 { lib, wayland, wayland-protocols, xorgserver, xkbcomp, xkeyboard_config
-, epoxy, libxslt, libunwind, makeWrapper, egl-wayland
-, defaultFontPath ? "" }:
+, epoxy, libtirpc, libxslt, libunwind, makeWrapper, egl-wayland, fetchurl, meson, ninja
+, pkg-config, fontutil, defaultFontPath ? "" }:
 
 with lib;
 
 xorgserver.overrideAttrs (oldAttrs: {
 
-  name = "xwayland-${xorgserver.version}";
-  buildInputs = oldAttrs.buildInputs ++ [ egl-wayland ];
+  name = "xwayland-21.0.99.902";
+  src = fetchurl {
+    url = "https://xorg.freedesktop.org/archive/individual/xserver/xwayland-21.0.99.902.tar.xz";
+    sha256 = "sha256-eZSR3ZnLLG298FFiRB0EaaKps24Il1XrWXKd049Erc4=";
+  };
+  patches = [ ];
+  nativeBuildInputs = oldAttrs.nativeBuildInputs ++ [ meson ninja ];
+  buildInputs = oldAttrs.buildInputs ++ [ egl-wayland fontutil libtirpc ];
   propagatedBuildInputs = oldAttrs.propagatedBuildInputs
     ++ [wayland wayland-protocols epoxy libxslt makeWrapper libunwind];
-  configureFlags = [
-    "--disable-docs"
-    "--disable-devel-docs"
-    "--enable-xwayland"
-    "--enable-xwayland-eglstream"
-    "--disable-xorg"
-    "--disable-xvfb"
-    "--disable-xnest"
-    "--disable-xquartz"
-    "--disable-xwin"
-    "--enable-glamor"
-    "--with-default-font-path=${defaultFontPath}"
-    "--with-xkb-bin-directory=${xkbcomp}/bin"
-    "--with-xkb-path=${xkeyboard_config}/etc/X11/xkb"
-    "--with-xkb-output=$(out)/share/X11/xkb/compiled"
+  mesonFlags = [
+    "-Dxwayland-eglstream=true"
+    "-Ddefault-font-path=${defaultFontPath}"
+    "-Dxkb_bin_dir=${xkbcomp}/bin"
+    "-Dxkb_dir=${xkeyboard_config}/etc/X11/xkb"
+    "-Dxkb_output_dir=$(out)/share/X11/xkb/compiled"
   ];
 
   postInstall = ''
